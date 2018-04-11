3a3fe8c5a9968cab73f5ee6b1dca857576730d18
Joao <joao.gc1997@gmail.com>
2018-04-11 15:11:45 -0300

    Banco Adicionado

--- ForLearning/ForLearning/settings.py
+++ ForLearning/ForLearning/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '3rgz_!9zp+#q&p$i=xxypzfe5f^e467n568yg-768dw2m#5@6m'
+SECRET_KEY = 'v9_(*n3-8d#0@d4pnhf4#)%#8r4@avwhagrt$oohvh!(tskwt+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
--- ForLearning/Glossario/apps.py
+++ ForLearning/Glossario/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class GlossarioConfig(AppConfig):
-    name = 'glossario'
+    name = 'Glossario'
