6a1de505acd98a3bf32f5cf3f4a86f0490620e89
Joao <joao.gc1997@gmail.com>
2018-04-11 16:23:33 -0300

    Rotas adicionadas

--- ForLearning/Glossario/urls.py
+++ ForLearning/Glossario/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
+    path('<int:categoria_id>/', views.conteudo_categoria, name='conteudo-categoria'),
+    path('<int:categoria_id>/enviar-sinal/', views.enviar_sinal, name='enviar-sinal'),
+    path('sinal-enviado-com-sucesso/', views.sinal_enviado_sucesso, name='sinal-enviado-sucesso'),
 ]
--- ForLearning/Glossario/views.py
+++ ForLearning/Glossario/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render, HttpResponse
+from django.template import loader
+from django.http import Http404
 
+from .models import Categoria
 # Create your views here.
 
 def index(request):
-    return HttpResponse("Começo de tudo")
+    lista_ultimas_categorias = Categoria.objects.order_by('-pub_date')[:5]
+    context = {'lista_ultimas_categorias': lista_ultimas_categorias}
+    return render(request, 'Glossario/index.trabalho.html', context)
+
+def conteudo_categoria(request, categoria_id):
+    try:
+        categoria = Categoria.objects.get(pk=categoria_id)
+    except Categoria.DoesNotExist:
+        raise Http404("Categoria não existe")
+    return render(request,'Glossario/index.trabalho.html', {'categoria':categoria})
+
+def enviar_sinal(request, categoria_id):
+    return HttpResponse("Enviar Sinal para categoria %s" % categoria_id)
+
+def sinal_enviado_sucesso(request):
+    return HttpResponse("Sinal enviado com sucesso")
