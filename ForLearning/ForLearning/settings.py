c10115ff859a77ecbad634b8b95c3ba8e5ea9f52
Joao <joao.gc1997@gmail.com>
2018-04-11 15:31:20 -0300

    Pastas static e template adicionadas

--- ForLearning/ForLearning/settings.py
+++ ForLearning/ForLearning/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'Glossario.apps.GlossarioConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
--- ForLearning/Glossario/models.py
+++ ForLearning/Glossario/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
 # Create your models here.
+
+class Categoria(models.Model):
+    titulo = models.CharField(max_length=200)
+    
+    def __str__(self):
+        return self.titulo
+        
+class Sinal(models.Model):
+    categoria = models.ForeignKey(Categoria, on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    descricao = models.TextField(blank=True)
+    
+    def __str__(self):
+            return self.titulo
