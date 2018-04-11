8a50ef8dcfd0c922a242d048c1c6dafb08b26449
Joao <joao.gc1997@gmail.com>
2018-04-11 17:26:29 -0300

    Modificações nas views e urls, agora é permitido o administrador adicionar sinais

--- ForLearning/Glossario/views.py
+++ ForLearning/Glossario/views.py
@@ -2,20 +2,22 @@ from django.shortcuts import render, HttpResponse
 from django.template import loader
 from django.http import Http404
 
-from .models import Categoria
+from .models import Categoria, Sinal
 # Create your views here.
 
 def index(request):
-    lista_ultimas_categorias = Categoria.objects.order_by('-pub_date')[:5]
-    context = {'lista_ultimas_categorias': lista_ultimas_categorias}
+    lista_categorias = Categoria.objects.all()
+    context = {'lista_categorias': lista_categorias}
     return render(request, 'Glossario/index.trabalho.html', context)
 
 def conteudo_categoria(request, categoria_id):
     try:
         categoria = Categoria.objects.get(pk=categoria_id)
+        lista_sinais = Sinal.objects.all()
+        context = {'lista_sinais': lista_sinais}
     except Categoria.DoesNotExist:
         raise Http404("Categoria não existe")
-    return render(request,'Glossario/index.trabalho.html', {'categoria':categoria})
+    return render(request,'Glossario/video1.html', {'categoria':categoria}, context)
 
 def enviar_sinal(request, categoria_id):
     return HttpResponse("Enviar Sinal para categoria %s" % categoria_id)
