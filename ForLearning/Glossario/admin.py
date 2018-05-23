26d5aedefb23b2b9fe6948460ec9a11655800219
joaoGrasel <joao.gc1997@gmail.com>
2018-05-23 17:01:01 -0300

    Model Temas adicionado ao projeto, agora os sinais possuem varios temas

--- ForLearning/Glossario/admin.py
+++ ForLearning/Glossario/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Glossario, Sinal
+from .models import Glossario, Sinal, Tema
 
 # Register your models here.
 
 admin.site.register(Glossario)
 admin.site.register(Sinal)
+admin.site.register(Tema)
