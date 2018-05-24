51409bf775adf797aa90d355d70fa6c57b886b57
Cleberton Oliveira <clebertonoliveira.cnt@gmail.com>
2018-05-24 16:45:14 -0300

    FIX

--- ForLearning/Glossario/urls.py
+++ ForLearning/Glossario/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('<int:glossario_id>/', views.conteudo_glossario, name='conteudo-glossario'),
     path('<int:tema_id>/', views.conteudo_tema, name='conteudo-tema'),
     path('<int:glossario_id>/enviar-sinal/', views.enviar_sinal, name='enviar-sinal')
+    path('<int:tema_id>/categorias-temas/', views.categorias_temas, name='categorias-temas')
+    path('<int:glossario_id>/categorias-glossario/', views.conteudo_glossario, name='categorias-glossario')
 ]
