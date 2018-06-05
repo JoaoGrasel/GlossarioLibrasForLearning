89173d7307b8237c2fd9fde60a332e56355636e4
joaoGrasel <joao.gc1997@gmail.com>
2018-06-05 15:40:59 -0300

    Estrutura de Login e Registro funcionando, feias mas funcionando

--- ForLearning/Usuarios/urls.py
+++ ForLearning/Usuarios/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-	path('registrar/', views.registrar_usuario_view, name="registrar"),
-    path('login/',views.login_view, name="login"),
+	path('registrar/', views.registrar, name="registrar"),
+    path('login/',views.logar, name="login"),
 	# path(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/login'}, name="logout")
 ]
--- ForLearning/Usuarios/views.py
+++ ForLearning/Usuarios/views.py
@@ -1,43 +1,29 @@
-from django.shortcuts import redirect, render
-from django.contrib.auth.models import User
-from django.views.generic.base import View
-from Glossario.models import Perfil
-from Usuarios.forms import RegistrarUsuarioForm, FormularioLogin
-from django.contrib.auth import authenticate, login
+from django.shortcuts import render_to_response, render
+from django.http import HttpResponseRedirect
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm
+from django.contrib.auth import login
 
 
-def login_view(request):
+def logar(request):
+	if request.method == 'POST':
+		formulario = AuthenticationForm(data=request.POST)
 
-	if request.method == "POST":
-		formulario = FormularioLogin(request.POST)
 		if formulario.is_valid():
-			dados_form = formulario.data
-			username = dados_form['username']
-			senha = dados_form['senha']
-			user = authenticate(username=username, password=senha)
-			if user is not None:
-				return redirect(request, 'index')
-			else:
-				return render(request, 'Usuarios/login.html')
-
+			login(request, formulario.get_user())
+			return HttpResponseRedirect("/")
 		else:
-			formulario = FormularioLogin()
-			return render(request, 'Usuarios/login.html')
+			return render(request, "Usuarios/login.html", {"formulario":formulario})
+	return render(request, "Usuarios/login.html", {"formulario": AuthenticationForm()})
 
-def registrar_usuario_view(request):
-	if request.method == "POST":		
-		formulario = RegistrarUsuarioForm(request.POST)
+def registrar(request):
+	if request.method=='POST':
+		formulario = UserCreationForm(request.POST)
+
 		if formulario.is_valid():
-			dados_form = formulario.data
-			usuario = User.objects.create_user(dados_form['username'], 
-							 				   dados_form['email'], 
-							 				   dados_form['senha'])
-			perfil = Perfil(nome=dados_form['nome'], 
-							usuario=usuario)
-			perfil.save()
-			return redirect('index')	
-		else: 
-			return render(request, 'Usuarios/registrar.html') 
-	else: 
-		formulario = RegistrarUsuarioForm()
-		return render(request, 'Usuarios/registrar.html') 
+			formulario.save()
+			return HttpResponseRedirect("/Usuarios/login/")
+		else:
+			return render(request, "Usuarios/registrar.html", {"formulario": formulario})
+
+	return render(request, "Usuarios/registrar.html", {"formulario": UserCreationForm()})		
