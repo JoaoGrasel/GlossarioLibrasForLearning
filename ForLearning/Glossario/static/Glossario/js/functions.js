a51b011876c9d36beeeac700c523ca2e98868501
Cleberton Oliveira <clebertonoliveira.cnt@gmail.com>
2018-06-06 15:50:38 -0300

    Templetes de login e registrar usuario

--- ForLearning/Glossario/static/Glossario/js/functions.js
+++ ForLearning/Glossario/static/Glossario/js/functions.js
@@ -1,7 +1,3 @@
-  document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.fixed-action-btn');
-    var instances = M.FloatingActionButton.init(elems, {
-      direction: 'left',
-      hoverEnabled: false
-    });
-  });
+$(document).ready(function(){
+    $('.sidenav').sidenav();
+});
