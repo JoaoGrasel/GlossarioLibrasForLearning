44ef3c9960d02563e9ce3b77cd7606f0fa96157b
joaoGrasel <joao.gc1997@gmail.com>
2018-06-06 15:46:14 -0300

    REGISTRO FUNCIONANDO 100% VAMO TIME, AGORA SIM, ESTAMOS VOOOOANDO DEMAIS, OLHA O ORGULHO RAMON

--- ForLearning/Usuarios/urls.py
+++ ForLearning/Usuarios/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 urlpatterns = [
 	path('registrar-usuario/', views.registrar_usuario, name="registrar_usuario"),
-	path('registrar-perfil/', views.registrar_perfil, name="registrar_perfil"),
     path('login/',views.logar, name="login"),
 	# path(r'^logout/$', 'django.contrib.auth.views.logout_then_login', {'login_url': '/login'}, name="logout")
 ]
