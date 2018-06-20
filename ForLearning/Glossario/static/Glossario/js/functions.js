2e91defce59c092a51a70486c08d400f0108d390
Cleberton-Oliveira <clebertonoliveira.cnt@gmail.com>
2018-06-20 07:11:13 -0300

    Mudanças esteticas na pagina index e criação da area de sinal com videos com descrição

--- ForLearning/Glossario/static/Glossario/js/functions.js
+++ ForLearning/Glossario/static/Glossario/js/functions.js
@@ -1,4 +1,4 @@
-  $(document).ready(function() {
+ $(document).ready(function() {
     $("#MostrarEsconderMensagem").click(MostrarEsconderMensagem);
  });
  
@@ -6,3 +6,25 @@
 function MostrarEsconderMensagem(){
     $("#Mensagem").toggle();
 }
+
+
+$('#glossario').mousemove(function(){
+    $(this).attr('src','../imagens/glossario.jpg');
+});
+
+$(function(){  
+    $("#glossario").hover(
+        function(){$(this).attr("src", "../../static/Glossario/imagens/glossario.jpg")},
+        function(){$(this).attr("src", "../../static/Glossario/imagens/glossario1.jpg")}
+    );
+
+     $("#pasta").hover(
+        function(){$(this).attr("src", "../../static/Glossario/imagens/pasta.jpg")},
+        function(){$(this).attr("src", "../../static/Glossario/imagens/pasta1.jpg")}
+    );
+
+
+
+});
+
+ 
